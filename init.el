d891b7646130d0168a6d0be08beff1c0fe318176
viksit <viksit@gmail.com>
2021-04-14 18:09:15 -0700

    clean up init.el for js improvements

--- init.el
+++ init.el
@@ -302,77 +302,13 @@ by Prelude.")
 
 
 ;; Typescript and javascript stuff
-;; (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) ;; auto-enable for .js/.jsx files
-;; (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
-
-;; (defun web-mode-init-hook ()
-;;   "Hooks for Web mode.  Adjust indent."
-;;   (setq web-mode-markup-indent-offset 2))
-;; (add-hook 'web-mode-hook  'web-mode-init-hook)
-
-;; typescript settings
-;; (setq-default typescript-indent-level 2)
-;; (add-to-list 'auto-mode-alist '("\\.tsx?$" . typescript-mode)) ;; auto-enable for .ts/.tsx files
-;; (add-to-list 'auto-mode-alist '("\\.js?$" . typescript-mode)) ;; auto-enable for .ts/.tsx files
-;; (setq typescript-mode-content-types-alist '(("tsx" . "\\.ts[x]?\\'")))
-;; (setq typescript-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
-
-(setq package-check-signature nil)
-
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
-;; enable typescript-tslint checker
-(flycheck-add-mode 'typescript-tslint 'web-mode)
-
-(add-hook 'js2-mode-hook #'setup-tide-mode)
-;; configure javascript-tide checker to run after your default javascript checker
-(flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
-
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "jsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
-;; configure jsx-tide checker to run after your default jsx checker
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-(flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
-
-;; (use-package tide
-;;              :ensure t
-;;              :after (typescript-mode company flycheck)
-;;              :hook ((typescript-mode . tide-setup)
-;;                     (typescript-mode . tide-hl-identifier-mode)
-;;                     (before-save . tide-format-before-save)))
+;; We want to use - rjsx mode for all JS files
 
 
 
 
-
+;; End JS/TS stuff
+(show-paren-mode)
 ;; end @viksit's edits
 
 
